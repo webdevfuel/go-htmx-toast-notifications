73337b39b99e9e36ef9ba1031dc459c4f207d99a
Emanuel Turis <hello@emanuelturis.com>
2024-07-25 19:00:20 +0300

    set up templ with hello, world!

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/webdevfuel/go-htmx-toast-notifications
 
 go 1.22.5
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require (
+	github.com/a-h/templ v0.2.747 // indirect
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/webdevfuel/go-htmx-toast-notifications/template"
 )
 
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("Hello, world!"))
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		component := template.Hello("world")
+		component.Render(r.Context(), w)
 	})
 	http.ListenAndServe("localhost:3000", r)
 }
