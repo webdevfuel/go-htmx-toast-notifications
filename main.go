152c10fe4207e409df7e1e9f7f5e5cad8b57d5f1
Emanuel Turis <hello@emanuelturis.com>
2024-07-25 19:16:24 +0300

    server static files from static directory

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		component := template.Hello("world")
 		component.Render(r.Context(), w)
